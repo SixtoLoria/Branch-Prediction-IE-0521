01bc2f3a193e2c2a2e56be74644af289e8da3d60
edwincamo <edwincamo9398@gmail.com>
2024-04-30 00:15:30 -0600

    Implementación de P-Share

--- gshared.py
+++ gshared.py
@@ -1,19 +1,5 @@
 class gshared:
-    def __init__(self, bits_to_index, global_history_size):
-        self.bits_to_index = bits_to_index
-        self.size_of_branch_table = 2**bits_to_index
-        self.global_history_size = global_history_size
-        self.max_index_global_history = 2**self.global_history_size
-        #First index with PC, second index with GHR
-        self.branch_table = [0 for i in range(self.size_of_branch_table)]
-
-        if self.global_history_size > self.bits_to_index:
-            print("El tamaño del registro de historia es mayor que los bits para indexar la tabla se limitará el registro a "+str(self.bits_to_index)+ "bits")
-            self.global_history_size = self.bits_to_index
-
-        self.global_history_reg = ""
-        for i in range(global_history_size):
-            self.global_history_reg += "0"
+    def __init__(self):
         self.total_predictions = 0
         self.total_taken_pred_taken = 0
         self.total_taken_pred_not_taken = 0
@@ -24,8 +10,6 @@ class gshared:
     def print_info(self):
         print("Parámetros del predictor:")
         print("\tTipo de predictor:\t\t\t\tG-Shared")
-        print("\tEntradas en el Predictor:\t\t\t"+str(2**self.bits_to_index))
-        print("\tTamaño de los registros de historia global:\t"+str(self.global_history_size))
 
     def print_stats(self):
         print("Resultados de la simulación")
@@ -39,66 +23,13 @@ class gshared:
         print("\t% predicciones correctas:\t\t\t\t"+str(formatted_perc)+"%")
 
     def predict(self, PC):
-        PC_index = int(PC) % self.size_of_branch_table
-        GHR_index = int(self.global_history_reg,2)
-        table_index = PC_index ^ GHR_index
-
-        #print("Predict")
-        #print(PC_index,GHR_index,table_index)
-        #print(bin(PC_index),bin(GHR_index),bin(table_index))
-
-        branch_table_entry = self.branch_table[table_index]
-
-        #print(branch_table_entry)
-
-        if branch_table_entry in [0,1]:
-            return "N"
-        else:
-            return "T"
+        #Escriba aquí el código para predecir
+        #La siguiente línea es solo para que funcione la prueba
+        #Quítela para implementar su código
+        return "T"
 
     def update(self, PC, result, prediction):
-        PC_index = int(PC) % self.size_of_branch_table
-        GHR_index = int(self.global_history_reg,2)
-        table_index = PC_index ^ GHR_index
-
-        #print("Update")
-        #print(PC_index,GHR_index,table_index)
-        #print(bin(PC_index),bin(GHR_index),bin(table_index))
-
-        branch_table_entry = self.branch_table[table_index]
-        #print(branch_table_entry)
-
-        #Update entry accordingly
-        if branch_table_entry == 0 and result == "N":
-            updated_branch_table_entry = branch_table_entry
-            #print(PC,GHR_index,branch_table_entry,updated_branch_table_entry,result,prediction)
-        elif branch_table_entry != 0 and result == "N":
-            updated_branch_table_entry = branch_table_entry - 1
-            #print(PC,GHR_index,branch_table_entry,updated_branch_table_entry,result,prediction)
-        elif branch_table_entry == 3 and result == "T":
-            updated_branch_table_entry = branch_table_entry
-            #print(PC,GHR_index,branch_table_entry,updated_branch_table_entry,result,prediction)
-        else:
-            updated_branch_table_entry = branch_table_entry + 1
-            #print(PC,GHR_index,branch_table_entry,updated_branch_table_entry,result,prediction)
-        self.branch_table[table_index] = updated_branch_table_entry
-        #print(branch_table_entry)
-
-        #Update GHR
-        if result == "T":
-            self.global_history_reg = self.global_history_reg[-self.global_history_size+1:] + "1"
-        else:
-            self.global_history_reg = self.global_history_reg[-self.global_history_size+1:] + "0"
-        #print("GHR = "+self.global_history_reg)
-
-        #Update stats
-        if result == "T" and result == prediction:
-            self.total_taken_pred_taken += 1
-        elif result == "T" and result != prediction:
-            self.total_taken_pred_not_taken += 1
-        elif result == "N" and result == prediction:
-            self.total_not_taken_pred_not_taken += 1
-        else:
-            self.total_not_taken_pred_taken += 1
-
-        self.total_predictions += 1
+        #Escriba aquí el código para actualizar
+        #La siguiente línea es solo para que funcione la prueba
+        #Quítela para implementar su código
+        a = PC
--- pshared.py
+++ pshared.py
@@ -1,6 +1,16 @@
 class pshared:
-    def __init__(self):
+    def __init__(self,bits_to_index, global_history_size):
         #Escriba aquí el init de la clase
+        self.bits_to_index = bits_to_index     #Bits del PC
+        self.global_history_size = global_history_size #bits de los registros de la tabla de historia global
+        self.size_global_history_table = 2**global_history_size #Tamaño de la tabla global
+        self.size_of_branch_table = 2**bits_to_index #Tamaño de la tabla del branch
+        
+        # Crear la tabla de ramas (branch table) con registros del tamaño de la historia global inicializados en ceros
+        self.branch_table = [0 for i in range(self.size_of_branch_table)]
+        self.global_history_table = [0 for i in range(self.size_global_history_table)]
+        
+
         self.total_predictions = 0
         self.total_taken_pred_taken = 0
         self.total_taken_pred_not_taken = 0
@@ -9,7 +19,10 @@ class pshared:
 
     def print_info(self):
         print("Parámetros del predictor:")
-        print("\tTipo de predictor:\t\t\tPerceptron")
+        print("\tTipo de predictor:\t\t\tP-Share")
+        print("\tTamaño de la tabla de patrones:\t" + str(self.size_of_branch_table))
+        print("\tTamaño del registro de historia global:\t" + str(self.global_history_size))
+        print("\tTamaño de la tabla de la historia global:\t" + str(self.size_global_history_table))
 
     def print_stats(self):
         print("Resultados de la simulación")
@@ -23,14 +36,56 @@ class pshared:
         print("\t% predicciones correctas:\t\t\t\t"+str(formatted_perc)+"%")
 
     def predict(self, PC):
-        #Escriba aquí el código para predecir
-        #La siguiente línea es solo para que funcione la prueba
-        #Quítela para implementar su código
-        return "T"
-  
+        index = int(PC) % self.size_of_branch_table
+        branch_table_entry = self.branch_table[index]
+        global_table_entry = self.global_history_table[branch_table_entry]
+        if global_table_entry in [2,3]:
+            return "T"
+        else:
+            return "N"
 
-    def update(self, PC, result, prediction):
-        #Escriba aquí el código para actualizar
-        #La siguiente línea es solo para que funcione la prueba
-        #Quítela para implementar su código
-        a = PC
+    def update(self, PC, result,prediction):
+        index = int(PC) % self.size_of_branch_table
+        branch_table_entry = self.branch_table[index]
+        global_table_entry = self.global_history_table[branch_table_entry]
+
+        # Update table
+        if (result == "N" and global_table_entry != 0 ):
+            updated_global_table_entry = global_table_entry - 1
+        
+                       
+        elif (result == "T" and global_table_entry ==3):
+            updated_global_table_entry = global_table_entry
+
+        elif (result == "N" and global_table_entry == 0):
+
+            updated_global_table_entry = global_table_entry
+        else:
+            updated_global_table_entry = global_table_entry + 1
+        #Actualizo la tabla de historia global con los resultados anteriores
+        self.global_history_table[branch_table_entry] = updated_global_table_entry 
+
+        # Update stats
+    #Update stats
+        if result == "T" and result == prediction:
+            self.total_taken_pred_taken += 1
+            self.branch_table[index] =self.branch_table[index] << 1
+            self.branch_table[index] += 1
+
+        elif result == "T" and result != prediction:
+            self.total_taken_pred_not_taken += 1
+            self.branch_table[index] =self.branch_table[index] << 1
+            self.branch_table[index] += 1
+
+        elif result == "N" and result == prediction:
+            self.total_not_taken_pred_not_taken += 1
+            self.branch_table[index] =self.branch_table[index] << 1
+            
+
+        else:
+            self.total_not_taken_pred_taken += 1
+            self.branch_table[index] =self.branch_table[index] << 1
+
+        self.total_predictions += 1
+        self.branch_table[index]=self.branch_table[index]%self.size_global_history_table
+ 

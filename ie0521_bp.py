98a226579dca65fea03f92d276c180b50bfec73d
Sixto Loria <sixtoloria@gmail.com>
2024-05-02 19:22:51 -0600

    implementacion de predictor personalizado

--- ie0521_bp.py
+++ ie0521_bp.py
@@ -1,16 +1,16 @@
 class ie0521_bp:
     def __init__(self):
-        #Escriba aquí el init de la clase
         self.total_predictions = 0
         self.total_taken_pred_taken = 0
         self.total_taken_pred_not_taken = 0
         self.total_not_taken_pred_taken = 0
         self.total_not_taken_pred_not_taken = 0
+        self.branch_history_table = {}  # Tabla para almacenar el historial de las predicciones
         
 
     def print_info(self):
         print("Parámetros del predictor:")
-        print("\tTipo de predictor:\t\t\tNombre de su predictor")
+        print("\tTipo de predictor:\t\t\tPredictor de bifurcación de dos bits")
 
     def print_stats(self):
         print("Resultados de la simulación")
@@ -24,14 +24,33 @@ class ie0521_bp:
         print("\t% predicciones correctas:\t\t\t\t"+str(formatted_perc)+"%")
 
     def predict(self, PC):
-        #Escriba aquí el código para predecir
-        #La siguiente línea es solo para que funcione la prueba
-        #Quítela para implementar su código
-        return "T"
+        if PC not in self.branch_history_table:
+            return "T"  # Predice que se tomará si no hay historial
+        elif self.branch_history_table[PC] > 1:
+            return "T"  # Predice que se tomará si el contador es 2 o 3
+        else:
+            return "N"  # Predice que no se tomará si el contador es 0 o 1
   
 
     def update(self, PC, result, prediction):
-        #Escriba aquí el código para actualizar
-        #La siguiente línea es solo para que funcione la prueba
-        #Quítela para implementar su código
-        a = PC
+        if PC not in self.branch_history_table:
+            self.branch_history_table[PC] = 2 if result == "T" else 0
+        else:
+            if result == "T":
+                if self.branch_history_table[PC] < 3:
+                    self.branch_history_table[PC] += 1
+            else:
+                if self.branch_history_table[PC] > 0:
+                    self.branch_history_table[PC] -= 1
+
+        self.total_predictions += 1
+        if result == "T":
+            if prediction == "T":
+                self.total_taken_pred_taken += 1
+            else:
+                self.total_taken_pred_not_taken += 1
+        else:
+            if prediction == "T":
+                self.total_not_taken_pred_taken += 1
+            else:
+                self.total_not_taken_pred_not_taken += 1

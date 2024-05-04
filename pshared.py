df6ecb87b4831f116ac0cacdd807160ee68eec97
Sixto Loria <sixtoloria@gmail.com>
2024-05-03 20:04:46 -0600

    Comentarios de P-share

--- pshared.py
+++ pshared.py
@@ -1,16 +1,23 @@
+"""
+Tarea01 Branch predictor.
+Estudiantes: 
+            Sixto Loria Villagra.         C04417
+            Edwin Camacho Mora.           B21304
+
+Grupo: 12
+"""
+
 class pshared:
     def __init__(self,bits_to_index, global_history_size):
-        #Escriba aquí el init de la clase
-        self.bits_to_index = bits_to_index     #Bits del PC
-        self.global_history_size = global_history_size #bits de los registros de la tabla de historia global
-        self.size_global_history_table = 2**global_history_size #Tamaño de la tabla global
-        self.size_of_branch_table = 2**bits_to_index #Tamaño de la tabla del branch
+        self.bits_to_index = bits_to_index                          #Bits del PC
+        self.global_history_size = global_history_size              #bits de los registros de la tabla de historia global
+        self.size_global_history_table = 2**global_history_size     #Tamaño de la tabla global
+        self.size_of_branch_table = 2**bits_to_index                #Tamaño de la tabla del branch
         
         # Crear la tabla de ramas (branch table) con registros del tamaño de la historia global inicializados en ceros
         self.branch_table = [0 for i in range(self.size_of_branch_table)]
         self.global_history_table = [0 for i in range(self.size_global_history_table)]
         
-
         self.total_predictions = 0
         self.total_taken_pred_taken = 0
         self.total_taken_pred_not_taken = 0
@@ -18,6 +25,13 @@ class pshared:
         self.total_not_taken_pred_not_taken = 0
 
     def print_info(self):
+        """ Funcion que se encarga de imprimir informacion general del predictor.
+        
+        Tales como los valores elegidos de tamaño de registros de historia y bits
+        del PC de index.
+        
+        Esta funcion no retorna ningun valor ni recibe ningun parametro.
+        """
         print("Parámetros del predictor:")
         print("\tTipo de predictor:\t\t\t\tP-Share")
         print("\tTamaño de la tabla de patrones:\t\t\t" + str(self.size_of_branch_table))
@@ -25,6 +39,13 @@ class pshared:
         print("\tTamaño de la tabla de la historia global:\t" + str(self.size_global_history_table))
 
     def print_stats(self):
+        """ Funcion que se encarga de imprimir los resultados del predictor
+        
+        Imprime los resultados mas releventes como los saltos tomados, todal de predicciones,
+        porcentaje de aciertos y entre otros.
+        
+        Esta funcion no retorna ningun valor ni recibe ningun parametro.
+        """
         print("Resultados de la simulación")
         print("\t# branches:\t\t\t\t\t\t"+str(self.total_predictions))
         print("\t# branches tomados predichos correctamente:\t\t"+str(self.total_taken_pred_taken))
@@ -36,15 +57,45 @@ class pshared:
         print("\t% predicciones correctas:\t\t\t\t"+str(formatted_perc)+"%")
 
     def predict(self, PC):
+        """  implementa el algoritmo de prediccion.
+    
+        Parameters
+        ----------
+        PC : int
+            Valor de bits del PC para indexar.
+        Returns
+        -------
+        T
+            String que indica que el salto fue tomado.
+        N
+            String que indica que el salto no fue tomado.
+        """
         index = int(PC) % self.size_of_branch_table
-        branch_table_entry = self.branch_table[index] #Tomo el contenido de acuerdo al indice
-        global_table_entry = self.global_history_table[branch_table_entry] #Tomo el contenido de acuerdo brancn_table_enry
-        if global_table_entry in [2,3]: # Rango entre 2 y 3 retorna T sino N 
+        branch_table_entry = self.branch_table[index]                       #Tomo el contenido de acuerdo al indice
+        global_table_entry = self.global_history_table[branch_table_entry]  #Tomo el contenido de acuerdo brancn_table_enry
+        if global_table_entry in [2,3]:                                     # Rango entre 2 y 3 retorna T sino N 
             return "T"
         else:
             return "N"
 
     def update(self, PC, result,prediction):
+        """ Se encarga de actualizar los valores del predictor.
+        
+        Parameters
+        ----------
+        PC : int
+            Valor de bits del PC para indexar.
+        result : String
+            Contiene el resultado de si se tomo o no el salto
+        prediction : String
+            Contiene el valor de N o T que eligio nuestro algorito para predecir.
+        
+        Returns
+        -------
+            no retorna ningun valor por variable, solo actualiza valores.
+        
+        Esta funcion no retorna ningun valor ni recibe ningun parametro.
+        """
         index = int(PC) % self.size_of_branch_table
         branch_table_entry = self.branch_table[index]
         global_table_entry = self.global_history_table[branch_table_entry]
@@ -85,7 +136,6 @@ class pshared:
             #Desplazo a la izquierda y inserto un 0 en el bit menos significativo
             self.branch_table[index] =self.branch_table[index] << 1
             
-
         else:
             #Aumento mis branches no tomados predichos incorrectamente
             self.total_not_taken_pred_taken += 1
